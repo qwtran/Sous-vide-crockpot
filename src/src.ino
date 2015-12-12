a0208254537981a4e99b9edb860d4d4bd90c8904
quan <a@test.com>
2015-12-11 21:30:23 -0800

    change source code

--- src/src.ino
+++ src/src.ino
@@ -1,38 +1,57 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+#define TARGET_TEMP 165
 #define ONE_WIRE_BUS 3
 #define RELAY_PIN  8
-#define TARGET 165
 #define DELAY 15000
 
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire(ONE_WIRE_BUS);  // Setup a oneWire instance to communicate with any OneWire devices
+DallasTemperature sensors(&oneWire);  // Pass our oneWire reference to Dallas Temperature. 
+DeviceAddress thermoAddress = { 0x28, 0xFF, 0xE3, 0xC8, 0x64, 0x15, 0x02, 0x6D }; // Setup themometer address
 
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
+boolean relaySwitchOn;
 
-// Setup themometer address
-DeviceAddress thermometer = { 0x28, 0xFF, 0xE3, 0xC8, 0x64, 0x15, 0x02, 0x6D };
+float Kp;
+float Ki = 0;
+float Kd = 0;
 
-// Is the relay on counter
-boolean relaySwitch;
+float input, output;
+float error, pastError, errorSum;
+unsigned long pastTime, currentTime;
+
 
 void setup(void)
 {
-  // Start serial port
   Serial.begin(9600);
-  
-  // Start up the library
-  sensors.begin();
+  sensors.begin();  // Start up the library
+  sensors.setResolution(thermoAddress, 10); // Set the resolution to 10 bit (good enough?)
 
-  // Set the resolution to 10 bit (good enough?)
-  sensors.setResolution(thermometer, 10);
-
-  // Set relay pin 8 to output pin
-  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT); // Set relay pin 8 to output pin
   digitalWrite(RELAY_PIN, HIGH);
-  relaySwitch = true;
+  relaySwitchOn = true;
+
+  pastError = errorSum = 0;
+}
+
+float calculatePID()
+{
+  float proportional, integral, derivative;
+
+  // Setup
+  pastTime = currentTime;
+  currentTime = millis();
+  pastError = error;
+  error = TARGET_TEMP - input;
+
+  proportional = Kp * error;
+
+  errorSum = errorSum + error * ( currentTime - pastTime );
+  integral = Ki * errorSum;
+  
+  derivative = Kd * ( error - pastError) / (currentTime - pastTime );
+
+  output = proportional + integral + derivative;
 }
 
 float getTemp(DeviceAddress deviceAddress)
@@ -46,25 +65,28 @@ float getTemp(DeviceAddress deviceAddress)
   }
 }
 
-void printData(float temp)
+void printData()
 {
-  Serial.print(millis()/1000);
+  Serial.print(currentTime/1000);
   Serial.print("\t");
-  Serial.print(temp);
+  Serial.print(input);
   Serial.print("\t");
-  Serial.print(relaySwitch);
+  Serial.print(relaySwitchOn);
+  Serial.print("\t");
+  Serial.print(output);
   Serial.print("\n");
 }
 
 void loop(void)
 {
-  float tempF = getTemp(thermometer);
-  printData(tempF);
+  input = getTemp(thermoAddress);
+  calculatePID();
+  printData();
 
-  if(tempF > TARGET)
+  if(input > TARGET_TEMP)
   {
     digitalWrite(RELAY_PIN, LOW);
-    relaySwitch = false;
+    relaySwitchOn = false;
   }
 
   delay(DELAY);
