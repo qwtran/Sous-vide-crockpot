089cd993e71bcb284c46820aed83e93bac1fd37a
Quan <git@qwtran.com>
2016-01-27 02:12:29 +0000

    code cleanup

--- web/server.py
+++ web/server.py
@@ -1,55 +1,52 @@
 import tornado.ioloop
 import tornado.web
 import time
-from tornado import gen
 import smbus
 import struct
 import sys
 
 pairs = {0:0}
+interval_ms = 1500
+
+# set up i2c connection
+address = 0x04
+bus = smbus.SMBus(1)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        global pairs
         for key, value in pairs.iteritems():
             self.write(str(key))
             self.write('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;')
             self.write(str(value))
             self.write('<p>')
 
-#@gen.engine
 def schedule_func():
-    global pairs
+    # request from i2c
     arrayData = bus.read_i2c_block_data(address,0x00)
-    print arrayData
+    # extract time from the first 32 bit
     time = arrayData[0]
     time |= arrayData[1] << 8
     time |= arrayData[2] << 16
     time |= arrayData[3] << 24
     time = time / 1000
+    # extract temp from the next 32 bit
     temp = arrayData[4:8] 
-    # time = struct.unpack('f', "".join(map(chr, time)))[0]
     temp = struct.unpack('f', "".join(map(chr, temp)))[0]
-    print time
-    print temp
+    # update with new data 
     pairs.update({time:temp})
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-
-if __name__ == "__main__":
-    bus = smbus.SMBus(1)
-    address = 0x04
-    app = make_app()
-    app.listen(8000)
-    #milliseconds
-    interval_ms = 15000
+def main():
+    # make main IOLoop
     main_loop = tornado.ioloop.IOLoop.instance()
     sched = tornado.ioloop.PeriodicCallback(schedule_func,interval_ms, io_loop = main_loop)
-    #start your period timer
+    # start periodic function
     sched.start()
-    #start your loop
+    # start web 
+    app = tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+    app.listen(8000)
     main_loop.start()
-    #tornado.ioloop.IOLoop.current().start()
+
+if __name__ == "__main__":
+    main()
