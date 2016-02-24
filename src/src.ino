9abdb28fe92bce6d67c118d3dcda9e2aa6fc268f
quan <a@test.com>
2016-02-24 00:59:52 -0800

    Change PID into a class

--- src/src.ino
+++ src/src.ino
@@ -1,7 +1,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define TARGET_TEMP 71
+#define TARGET_TEMP 65
 #define PID_INTERVAL 15000 // interval in milliseconds
 #define TEMP_INTERVAL 1000 // interval at which to read temp (milliseconds)
 
@@ -11,59 +11,68 @@
 
 unsigned long pastTemp, pastPID;
 
-static float initialIntegralError = 1700;   // Initialize controller integral error in msec
-static float integratorRange = 1;     // The maximum error allowable for integrator to be active
-
 OneWire ds(ONE_WIRE_BUS);  // Setup a oneWire instance to communicate with any OneWire devices
 byte temperatureAddress[8];
 
 class PID {
-  
+  public:
+    float Kp = 3000;
+    float Ki = .0025;
+    float Kd = 300000000;
+
+    float input, output;
+    float error, pastError, errorSum;
+    unsigned long pastTime, currentTime;
+    float proportional, integral, derivative;
+
+    float initialIntegralError = 1700;   // Initialize controller integral error in msec
+    float integratorRange = 1;     // The maximum error allowable for integrator to be active
+
+    PID() {
+      input = output = 0;
+      error = pastError = 0;
+      errorSum = initialIntegralError / Ki;
+      pastTime = currentTime = 0;
+      proportional = integral = derivative = 0;
+      pastTime = currentTime = 0;
+    }
+
+    void calculatePID() { // make into class with static variables in future
+      // PID setup
+      pastTime = currentTime;
+      currentTime = millis();
+      pastError = error;
+      error = TARGET_TEMP - input;
+
+      proportional = Kp * error;    // calculate the proportional term
+
+      if( (error >= (-1)*integratorRange) && (error <= integratorRange) ) {  // Only sum integrator term if error is within range
+        errorSum = errorSum + error * ( currentTime - pastTime );
+      }
+      integral = Ki * errorSum;
+
+      derivative = Kd * ((error - pastError) / (currentTime - pastTime));   // calculate the derivative term
+
+      output = proportional + integral + derivative;
+    }
 };
 
-float Kp = 3000;
-float Ki = .0025;
-float Kd = 300000000;
-
-float input, output;
-float error, pastError, errorSum;
-unsigned long pastTime, currentTime;
-
-float proportional, integral, derivative;
-
-float calculatePID() { // make into class with static variables in future
-  // PID setup
-  pastTime = currentTime;
-  currentTime = millis();
-  pastError = error;
-  error = TARGET_TEMP - input;
-
-  proportional = Kp * error;    // calculate the proportional term
-
-  if( (error >= (-1)*integratorRange) && (error <= integratorRange) ) {  // Only sum integrator term if error is within range
-    errorSum = errorSum + error * ( currentTime - pastTime );
-  }
-  integral = Ki * errorSum;
-  
-  derivative = Kd * ((error - pastError) / (currentTime - pastTime));   // calculate the derivative term
-
-  output = proportional + integral + derivative;
-}
+PID myPID = PID();
 
 void printData() {
-  Serial.print( ((currentTime/1000)%60)/100.0 + currentTime/60000);
+  Serial.print( ((myPID.currentTime/1000)%60)/100.0 + myPID.currentTime/60000);
   Serial.print("\t");
-  Serial.print(currentTime/1000);
+  Serial.print(myPID.currentTime/1000);
   Serial.print("\t");
-  Serial.print(input);
+  Serial.print(myPID.input);
   Serial.print("\t");
-  Serial.print(output/1000);
+  Serial.print(myPID.output/1000);
   Serial.print("\t");
-  Serial.print(proportional/1000);
+  Serial.print(myPID.proportional/1000);
   Serial.print("\t");
-  Serial.print(integral/1000);
+  Serial.print(myPID.integral/1000);
   Serial.print("\t");
-  Serial.print(derivative/1000);
+  Serial.print(myPID.derivative/1000);
   Serial.print("\n");
 }
 
@@ -99,38 +108,31 @@ void read_temp(byte temperatureAddress[8]) {
   Whole = Tc_100 / 100;  // separate off the whole and fractional portions
   Fract = Tc_100 % 100;
 
-  input = (float) Tc_100 / 100;
-  input = input * 1.8 + 32;
+  myPID.input = (float) Tc_100 / 100;
+  myPID.input = myPID.input * 1.8 + 32;
 }
 
 void setup(void) {
   Serial.begin(9600);
   pinMode(RELAY_PIN, OUTPUT); // Set relay pin 8 to output pin
 
-  ds.search(temperatureAddress);
-  send_for_temp(temperatureAddress);
-
-  input = output = 0;
-  error = pastError = 0;
-  errorSum = initialIntegralError / Ki;
-  pastTime = currentTime = 0;
-  proportional = integral = derivative = 0;
-  pastTime = currentTime = 0;
+  ds.search(temperatureAddress);  // Set up temperature
+  send_for_temp(temperatureAddress);  // Get temperature
 }
 
 void loop(void) {
-  if (millis() - pastTemp > TEMP_INTERVAL) {  // OVERFLOW????
+  if (millis() - pastTemp > TEMP_INTERVAL) {  // Update temp every interval
     pastTemp = millis();
-    read_temp(temperatureAddress);
-    send_for_temp(temperatureAddress);
+    read_temp(temperatureAddress);  // Get temp from device
+    send_for_temp(temperatureAddress);  // Ask device to update temperature for next read
   }
 
-  if(millis() - pastPID > PID_INTERVAL) { //time to shift the Relay Window
+  if(millis() - pastPID > PID_INTERVAL) { // Run PID every interval
     pastPID = pastPID + PID_INTERVAL;
-    calculatePID();
+    myPID.calculatePID();
     printData();
   }
-  if(millis() - pastPID < output)
+  if(millis() - pastPID < myPID.output)
     digitalWrite(RELAY_PIN,HIGH);
   else
     digitalWrite(RELAY_PIN,LOW);
